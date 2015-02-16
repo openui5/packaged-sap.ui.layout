15416d3ffaa75a7e19b07f589a9f31a90a6e9df1
OpenUI5 <openui5@sap.com>
2015-02-16 17:06:08 +0100

    1.26.7

--- resources/sap/ui/layout/GridRenderer.js
+++ resources/sap/ui/layout/GridRenderer.js
@@ -11,7 +11,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * @author SAP SE
 	 * @version
-	 * 1.26.6
+	 * 1.26.7
 	 * @namespace
 	 */
 	var GridRenderer = {};
--- resources/sap/ui/layout/form/ResponsiveLayout.js
+++ resources/sap/ui/layout/form/ResponsiveLayout.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/layout/ResponsiveFlowLayout', 'sap/u
 	 * @extends sap.ui.layout.form.FormLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.26.6
+	 * @version 1.26.7
 	 *
 	 * @constructor
 	 * @public
