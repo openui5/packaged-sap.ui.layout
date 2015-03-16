fa69a655bf87a4f2c2272ec7dbcd1487dd93e203
OpenUI5 <openui5@sap.com>
2015-03-16 10:42:20 +0100

    1.28.1

--- resources/sap/ui/layout/form/FormElement.js
+++ resources/sap/ui/layout/form/FormElement.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.28.0
+	 * @version 1.28.1
 	 *
 	 * @constructor
 	 * @public
