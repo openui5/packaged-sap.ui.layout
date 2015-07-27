7c462437513a99208149a0835105a4ed31ab1a74
OpenUI5 <openui5@sap.com>
2015-07-27 10:49:50 +0200

    1.30.3

--- resources/sap/ui/layout/FixFlex.js
+++ resources/sap/ui/layout/FixFlex.js
@@ -26,7 +26,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/EnabledP
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.30.2
+		 * @version 1.30.3
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/ui/layout/GridRenderer.js
+++ resources/sap/ui/layout/GridRenderer.js
@@ -11,7 +11,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * @author SAP SE
 	 * @version
-	 * 1.30.2
+	 * 1.30.3
 	 * @namespace
 	 */
 	var GridRenderer = {};
