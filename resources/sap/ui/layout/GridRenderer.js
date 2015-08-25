af694378cf58e317908fb49732e0c679e7a934a3
OpenUI5 <openui5@sap.com>
2015-08-25 14:15:38 +0200

    1.26.13

--- resources/sap/ui/layout/GridRenderer.js
+++ resources/sap/ui/layout/GridRenderer.js
@@ -1,5 +1,5 @@
 /*!
- * SAP UI development toolkit for HTML5 (SAPUI5/OpenUI5)
+ * UI development toolkit for HTML5 (OpenUI5)
  * (c) Copyright 2009-2015 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
@@ -11,7 +11,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * @author SAP SE
 	 * @version
-	 * 1.26.11
+	 * 1.26.13
 	 * @namespace
 	 */
 	var GridRenderer = {};
--- resources/sap/ui/layout/form/SimpleForm.js
+++ resources/sap/ui/layout/form/SimpleForm.js
@@ -1,5 +1,5 @@
 /*!
- * SAP UI development toolkit for HTML5 (SAPUI5/OpenUI5)
+ * UI development toolkit for HTML5 (OpenUI5)
  * (c) Copyright 2009-2015 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/layout/Respon
 	 * @class
 	 * Use the SimpleForm to create a form based on title, label and fields that are stacked in the content aggregation. Add Title to start a new FormContainer(Group). Add Label to start a new row in the container. Add Input/Display controls as needed. Use LayoutData to influence the layout for special cases in the Input/Display controls.
 	 * @extends sap.ui.core.Control
-	 * @version 1.26.11
+	 * @version 1.26.13
 	 *
 	 * @constructor
 	 * @public
