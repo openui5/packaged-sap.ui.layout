9dde0821845840d7815d3925270e17ada4356a00
OpenUI5 <openui5@sap.com>
2016-02-01 14:25:22 +0100

    1.32.11

--- resources/sap/ui/layout/DynamicSideContent.js
+++ resources/sap/ui/layout/DynamicSideContent.js
@@ -28,7 +28,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/ResizeHa
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.32.10
+		 * @version 1.32.11
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/ui/layout/GridRenderer.js
+++ resources/sap/ui/layout/GridRenderer.js
@@ -11,7 +11,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * @author SAP SE
 	 * @version
-	 * 1.32.10
+	 * 1.32.11
 	 * @namespace
 	 */
 	var GridRenderer = {};
