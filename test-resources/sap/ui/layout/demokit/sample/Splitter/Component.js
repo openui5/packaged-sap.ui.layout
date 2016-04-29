ad41b2d453fe7099179baf5c821b4f32d7aeca1e
OpenUI5 <openui5@sap.com>
2016-04-29 23:07:33 +0200

    1.38.0

---
+++ test-resources/sap/ui/layout/demokit/sample/Splitter/Component.js
@@ -0,0 +1,32 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.ui.layout.sample.Splitter.Component", {
+
+		metadata : {
+			rootView : "sap.ui.layout.sample.Splitter.Splitter",
+			includes : [
+				"css/splitter.css", // This is how it should be...
+				"Splitter/css/splitter.css"  // This is what works right now
+			],
+			dependencies : {
+				libs : [
+					"sap.ui.commons",
+					"sap.ui.layout"
+				]
+			},
+			config : {
+				sample : {
+					files : [
+						"Splitter.view.xml",
+						"Splitter.controller.js"
+					]
+				}
+			}
+		}
+	});
+
+	return Component;
+
+});
