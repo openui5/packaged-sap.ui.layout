d7be893071734c19834a78d706fbc08e374045da
OpenUI5 <openui5@sap.com>
2016-05-10 12:32:11 +0200

    1.28.34

--- resources/sap/ui/layout/form/ResponsiveLayout.js
+++ resources/sap/ui/layout/form/ResponsiveLayout.js
@@ -30,7 +30,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/layout/ResponsiveFlowLayout', 'sap/u
 	 * @extends sap.ui.layout.form.FormLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.28.33
+	 * @version 1.28.34
 	 *
 	 * @constructor
 	 * @public
