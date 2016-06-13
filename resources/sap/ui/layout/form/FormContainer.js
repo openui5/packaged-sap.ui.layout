4c9b1b19ad18ac934d087ba5471f715536b2a8d0
OpenUI5 <openui5@sap.com>
2016-06-13 15:59:59 +0200

    1.36.13

--- resources/sap/ui/layout/form/FormContainer.js
+++ resources/sap/ui/layout/form/FormContainer.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.36.12
+	 * @version 1.36.13
 	 *
 	 * @constructor
 	 * @public
