bd056d42f0cce20cd90c114a79740f0b143e6c73
OpenUI5 <openui5@sap.com>
2016-07-19 12:15:51 +0200

    1.40.0

---
+++ test-resources/sap/ui/layout/demokit/sample/FormToolbar/Component.js
@@ -0,0 +1,28 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.ui.layout.sample.FormToolbar.Component", {
+
+		metadata : {
+			rootView : "sap.ui.layout.sample.FormToolbar.Page",
+			dependencies : {
+				libs : [
+					"sap.m",
+					"sap.ui.layout"
+				]
+			},
+			config : {
+				sample : {
+					files : [
+						"Page.view.xml",
+						"Page.controller.js"
+					]
+				}
+			}
+		}
+	});
+
+	return Component;
+
+});
