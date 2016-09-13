c533425de3fcb992a38f7f538b8520c45a93a476
OpenUI5 <openui5@sap.com>
2016-09-13 12:38:37 +0200

    1.40.7

--- resources/sap/ui/layout/changeHandler/AddSimpleFormGroup.js
+++ resources/sap/ui/layout/changeHandler/AddSimpleFormGroup.js
@@ -13,7 +13,7 @@ sap.ui.define([
 	 * Change handler for adding a smart form group.
 	 * @alias sap.ui.fl.changeHandler.AddGroup
 	 * @author SAP SE
-	 * @version 1.40.6
+	 * @version 1.40.7
 	 * @experimental Since 1.27.0
 	 */
 	var AddGroup = { };
