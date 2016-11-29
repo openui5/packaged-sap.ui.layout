d3fbd848307047968e332e4fb7270854783061c9
OpenUI5 <openui5@sap.com>
2016-11-29 09:28:02 +0100

    1.40.12

--- resources/sap/ui/layout/ResponsiveSplitter.js
+++ resources/sap/ui/layout/ResponsiveSplitter.js
@@ -21,7 +21,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Control", "./Respo
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.40.11
+	 * @version 1.40.12
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/layout/changeHandler/AddSimpleFormGroup.js
+++ resources/sap/ui/layout/changeHandler/AddSimpleFormGroup.js
@@ -13,7 +13,7 @@ sap.ui.define([
 	 * Change handler for adding a smart form group.
 	 * @alias sap.ui.fl.changeHandler.AddGroup
 	 * @author SAP SE
-	 * @version 1.40.11
+	 * @version 1.40.12
 	 * @experimental Since 1.27.0
 	 */
 	var AddGroup = {};
