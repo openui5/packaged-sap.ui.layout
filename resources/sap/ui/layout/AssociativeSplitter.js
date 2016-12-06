6408c9fdde1405bd6becc4e5dd8adfe3138c37ed
OpenUI5 <openui5@sap.com>
2016-12-06 10:33:19 +0100

    1.44.2

--- resources/sap/ui/layout/AssociativeSplitter.js
+++ resources/sap/ui/layout/AssociativeSplitter.js
@@ -20,7 +20,7 @@ sap.ui.define(['./Splitter', './SplitterRenderer'],
 	 * @extends sap.ui.layout.Splitter
 	 *
 	 * @author SAP SE
-	 * @version 1.44.1
+	 * @version 1.44.2
 	 *
 	 * @constructor
 	 * @private
--- resources/sap/ui/layout/changeHandler/MoveSimpleForm.js
+++ resources/sap/ui/layout/changeHandler/MoveSimpleForm.js
@@ -13,7 +13,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/fl/changeHandler/JsControlTreeModifi
 			 *
 			 * @alias sap.ui.fl.changeHandler.MoveElements
 			 * @author SAP SE
-			 * @version 1.44.1
+			 * @version 1.44.2
 			 * @experimental Since 1.34.0
 			 */
 			var MoveSimpleForm = {};
