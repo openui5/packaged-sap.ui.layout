ba802fb44f09114b00c8de59e0df6984e2dfd684
OpenUI5 <openui5@sap.com>
2016-12-07 16:43:33 +0100

    1.38.14

--- resources/sap/ui/layout/AssociativeSplitter.js
+++ resources/sap/ui/layout/AssociativeSplitter.js
@@ -20,7 +20,7 @@ sap.ui.define(['./Splitter', './SplitterRenderer'],
 	 * @extends sap.ui.layout.Splitter
 	 *
 	 * @author SAP SE
-	 * @version 1.38.12
+	 * @version 1.38.14
 	 *
 	 * @constructor
 	 * @private
--- resources/sap/ui/layout/form/GridLayout.js
+++ resources/sap/ui/layout/form/GridLayout.js
@@ -29,7 +29,7 @@ sap.ui.define(['jquery.sap.global', './FormLayout', './GridContainerData', './Gr
 	 * @extends sap.ui.layout.form.FormLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.38.12
+	 * @version 1.38.14
 	 *
 	 * @constructor
 	 * @public
