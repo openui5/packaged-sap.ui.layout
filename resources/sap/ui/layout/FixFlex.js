38f2d58e5bfda6a8908c9f2222d630a7cf681fc8
OpenUI5 <openui5@sap.com>
2017-04-11 12:29:35 +0200

    1.46.6

--- resources/sap/ui/layout/FixFlex.js
+++ resources/sap/ui/layout/FixFlex.js
@@ -28,7 +28,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Control", "sap/ui/core/EnabledP
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.46.5
+		 * @version 1.46.6
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/ui/layout/SplitPane.js
+++ resources/sap/ui/layout/SplitPane.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Element'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.46.5
+	 * @version 1.46.6
 	 *
 	 * @constructor
 	 * @public
