9642c1ca6064ffc88f33350470c137d257f55a36
OpenUI5 <openui5@sap.com>
2017-05-22 13:52:49 +0200

    1.44.14

--- resources/sap/ui/layout/PaneContainer.js
+++ resources/sap/ui/layout/PaneContainer.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Element', './Split
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.13
+	 * @version 1.44.14
 	 *
 	 * @constructor
 	 * @public
