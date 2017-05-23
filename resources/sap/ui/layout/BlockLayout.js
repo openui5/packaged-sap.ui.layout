7d87e1077ed10e457bca78a623dcf6e6c07b9a61
OpenUI5 <openui5@sap.com>
2017-05-23 15:27:33 +0200

    1.38.22

--- resources/sap/ui/layout/BlockLayout.js
+++ resources/sap/ui/layout/BlockLayout.js
@@ -21,7 +21,7 @@ sap.ui.define(['sap/ui/core/Control', './library'],
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.38.21
+		 * @version 1.38.22
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/ui/layout/GridData.js
+++ resources/sap/ui/layout/GridData.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/LayoutData', './library'],
 	 * @extends sap.ui.core.LayoutData
 	 *
 	 * @author SAP SE
-	 * @version 1.38.21
+	 * @version 1.38.22
 	 *
 	 * @constructor
 	 * @public
