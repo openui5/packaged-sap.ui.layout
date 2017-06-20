4203875c218ea7da34eaea6dc9dcbe0f9b7f56a0
OpenUI5 <openui5@sap.com>
2017-06-20 14:51:24 +0200

    1.44.15

--- resources/sap/ui/layout/FixFlex.js
+++ resources/sap/ui/layout/FixFlex.js
@@ -28,7 +28,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Control", "sap/ui/core/EnabledP
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.44.14
+		 * @version 1.44.15
 		 *
 		 * @constructor
 		 * @public
