df5302c621138b6b256613287800934d40e8047d
OpenUI5 <openui5@sap.com>
2017-07-04 17:13:48 +0200

    1.44.17

--- resources/sap/ui/layout/SplitPane.js
+++ resources/sap/ui/layout/SplitPane.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Element'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.16
+	 * @version 1.44.17
 	 *
 	 * @constructor
 	 * @public
