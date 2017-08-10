de731548fbac2d0822419138dbcc9a57eeab597d
OpenUI5 <openui5@sap.com>
2017-08-10 07:27:47 +0200

    1.50.0

---
+++ test-resources/sap/ui/layout/demokit/sample/FixFlexMinFlexSize/Component.js
@@ -0,0 +1,30 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.ui.layout.sample.FixFlexMinFlexSize.Component", {
+
+		metadata: {
+			rootView: "sap.ui.layout.sample.FixFlexMinFlexSize.V",
+			dependencies: {
+				libs: [
+					"sap.ui.layout",
+					"sap.m"
+				]
+			},
+			config: {
+				sample: {
+					stretch: true,
+					files: [
+						"C.controller.js",
+						"V.view.xml"
+					]
+				}
+			}
+		}
+	});
+
+
+	return Component;
+
+});
