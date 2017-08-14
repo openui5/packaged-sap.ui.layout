dad16102d591728e2010eef2dff59422a2244f38
OpenUI5 <openui5@sap.com>
2017-08-14 17:22:59 +0200

    1.44.19

--- resources/sap/ui/layout/VerticalLayout.js
+++ resources/sap/ui/layout/VerticalLayout.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/EnabledP
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.44.18
+	 * @version 1.44.19
 	 *
 	 * @constructor
 	 * @public
