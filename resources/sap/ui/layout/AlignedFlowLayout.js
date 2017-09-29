be18c857aad6b53ebf085ad7e30184e68381201b
OpenUI5 <openui5@sap.com>
2017-09-29 16:34:57 +0200

    1.50.3

--- resources/sap/ui/layout/AlignedFlowLayout.js
+++ resources/sap/ui/layout/AlignedFlowLayout.js
@@ -24,7 +24,7 @@ sap.ui.define(['sap/ui/core/Control', './library', 'sap/ui/core/ResizeHandler'],
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.50.2
+		 * @version 1.50.3
 		 *
 		 * @constructor
 		 * @private
--- resources/sap/ui/layout/ResponsiveSplitterPage.js
+++ resources/sap/ui/layout/ResponsiveSplitterPage.js
@@ -21,7 +21,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Control"],
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.50.2
+	 * @version 1.50.3
 	 *
 	 * @constructor
 	 * @private
