38941753a4e39cedbec008d5f5ab57f9460deab7
OpenUI5 <openui5@sap.com>
2017-10-20 12:26:32 +0200

    1.52.0

---
+++ test-resources/sap/ui/layout/demokit/sample/FixFlexHorizontal/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.ui.layout.sample.FixFlexHorizontal.Component", {
+
+		metadata : {
+			rootView : "sap.ui.layout.sample.FixFlexHorizontal.V",
+			dependencies : {
+				libs : [
+				    "sap.ui.layout",
+					"sap.m"
+				]
+			},
+			includes : [ "FixFlexHorizontal/style.css" ],
+			config : {
+				sample : {
+					stretch : true,
+					files : [
+						"V.view.xml",
+						"C.controller.js",
+						"style.css"
+					]
+				}
+			}
+		}
+	});
+
+	return Component;
+
+});
