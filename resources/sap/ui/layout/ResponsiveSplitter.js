b98ac4ce00b44b119af53d6dbeb9b154ed891230
OpenUI5 <openui5@sap.com>
2017-11-14 16:34:20 +0100

    1.50.6

--- resources/sap/ui/layout/ResponsiveSplitter.js
+++ resources/sap/ui/layout/ResponsiveSplitter.js
@@ -42,7 +42,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Control", "./Respo
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.50.5
+	 * @version 1.50.6
 	 *
 	 * @constructor
 	 * @public
