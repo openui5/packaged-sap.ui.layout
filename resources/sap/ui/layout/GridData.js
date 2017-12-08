7d0de32f90102792dcdfd2c3149133a458ba682f
OpenUI5 <openui5@sap.com>
2017-12-08 19:59:47 +0100

    1.44.25

--- resources/sap/ui/layout/GridData.js
+++ resources/sap/ui/layout/GridData.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/LayoutData', './library'],
 	 * @extends sap.ui.core.LayoutData
 	 *
 	 * @author SAP SE
-	 * @version 1.44.24
+	 * @version 1.44.25
 	 *
 	 * @constructor
 	 * @public
