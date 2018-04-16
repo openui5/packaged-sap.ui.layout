ab2e6ff638ee76f934b9a5ab2d0d1ac8dffaf724
OpenUI5 <openui5@sap.com>
2018-04-16 14:49:02 +0200

    1.38.34

--- resources/sap/ui/layout/AssociativeSplitter.js
+++ resources/sap/ui/layout/AssociativeSplitter.js
@@ -20,7 +20,7 @@ sap.ui.define(['./Splitter', './SplitterRenderer'],
 	 * @extends sap.ui.layout.Splitter
 	 *
 	 * @author SAP SE
-	 * @version 1.38.33
+	 * @version 1.38.34
 	 *
 	 * @constructor
 	 * @private
--- resources/sap/ui/layout/PaneContainer.js
+++ resources/sap/ui/layout/PaneContainer.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Element', './Split
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.38.33
+	 * @version 1.38.34
 	 *
 	 * @constructor
 	 * @public
