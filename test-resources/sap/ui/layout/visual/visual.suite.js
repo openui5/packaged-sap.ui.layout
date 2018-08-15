4dafa63f7a59632a47369d705ed2b7096d98caf0
OpenUI5 <openui5@sap.com>
2018-08-15 13:32:37 +0200

    1.58.0

---
+++ test-resources/sap/ui/layout/visual/visual.suite.js
@@ -0,0 +1,10 @@
+/* global module */
+
+module.exports = [
+	"BlockLayout.spec.js",
+	"ResponsiveGridLayoutVisual.spec.js",
+	"ResponsiveLayoutVisual.spec.js",
+	"GridLayoutVisual.spec.js",
+	"ColumnLayoutVisual.spec.js",
+	"HorizontalLayout.spec.js"
+];
