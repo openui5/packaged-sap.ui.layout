1a329cbd287dc2afd8fc36bda44e82e8d28dd1ea
OpenUI5 <openui5@sap.com>
2018-09-26 17:08:36 +0200

    1.44.36

--- resources/sap/ui/layout/SplitPane.js
+++ resources/sap/ui/layout/SplitPane.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Element'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.35
+	 * @version 1.44.36
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/layout/changeHandler/UnhideSimpleForm.js
+++ resources/sap/ui/layout/changeHandler/UnhideSimpleForm.js
@@ -13,7 +13,7 @@ sap.ui.define([
 	 * Change handler for hiding of a control.
 	 * @alias sap.ui.fl.changeHandler.HideControl
 	 * @author SAP SE
-	 * @version 1.44.35
+	 * @version 1.44.36
 	 * @experimental Since 1.27.0
 	 */
 	var UnhideForm = { };
