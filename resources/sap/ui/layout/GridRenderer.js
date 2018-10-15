05b55c0861221a598144d67f69936ccb56560b32
OpenUI5 <openui5@sap.com>
2018-10-15 16:37:20 +0200

    1.58.4

--- resources/sap/ui/layout/GridRenderer.js
+++ resources/sap/ui/layout/GridRenderer.js
@@ -15,7 +15,7 @@ sap.ui.define(["sap/ui/Device", "sap/ui/layout/library"],
 	/**
 	 * @author SAP SE
 	 * @version
-	 * 1.58.3
+	 * 1.58.4
 	 * @namespace
 	 */
 	var GridRenderer = {};
--- resources/sap/ui/layout/changeHandler/MoveSimpleForm.js
+++ resources/sap/ui/layout/changeHandler/MoveSimpleForm.js
@@ -18,7 +18,7 @@ sap.ui.define([
 			 *
 			 * @alias sap.ui.fl.changeHandler.MoveElements
 			 * @author SAP SE
-			 * @version 1.58.3
+			 * @version 1.58.4
 			 * @experimental Since 1.34.0
 			 */
 			var MoveSimpleForm = {};
