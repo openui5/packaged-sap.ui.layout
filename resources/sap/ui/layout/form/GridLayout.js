408eed76a6baa07db15a76aea8651d1ded8c9975
OpenUI5 <openui5@sap.com>
2019-03-22 13:59:49 +0100

    1.44.40

--- resources/sap/ui/layout/form/GridLayout.js
+++ resources/sap/ui/layout/form/GridLayout.js
@@ -29,7 +29,7 @@ sap.ui.define(['jquery.sap.global', './FormLayout', './GridContainerData', './Gr
 	 * @extends sap.ui.layout.form.FormLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.44.39
+	 * @version 1.44.40
 	 *
 	 * @constructor
 	 * @public
